eb1b748bed01f970100a04da47d599232ca49f3c
krrn0722 <oomr0722@icloud.com>
2023-09-07 23:06:57 +0900

    userのpublic static 変数とボタンの追加

---
+++ Assets/Scripts/FirstScripts/StatrButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatrButton : MonoBehaviour
+{
+    // ボタンが押された時、名前を保存、ゲームスタート
+    public void OnClickGameStart()
+    {
+        Debug.Log("押された!");  // ログを出力
+    }
+}
---
+++ Assets/Scripts/FirstScripts/UserPublicScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class UserPublicScript : MonoBehaviour
+{
+    //別のシーンでも使用する
+    public static string userName = "";
+    public static int clearTime;
+}
