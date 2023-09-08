534a75a12bdcdbc9d6a7d7ab5753a9c6bc4ed466
krrn0722 <oomr0722@icloud.com>
2023-09-08 12:28:50 +0900

    データベースからランキングの取得とその表示

--- Assets/Scripts/FirstScripts/UserPublicScript.cs
+++ Assets/Scripts/FirstScripts/UserPublicScript.cs
@@ -7,6 +7,6 @@ using UnityEngine.SocialPlatforms.Impl;
 public class UserPublicScript : MonoBehaviour
 {
     //別のシーンでも使用する
-    public static string userName = "";
-    public static int clearTime;
+    public static string userName = "My";
+    public static double clearTime = 100;
 }
---
+++ Assets/Scripts/ResultScripts/ResultTextManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ResultTextManager : MonoBehaviour
+{
+    public TextMeshProUGUI myNameText;
+    public TextMeshProUGUI myTimeText; 
+
+    void Start()
+    {
+        //ユーザー名
+        // オブジェクトからTextコンポーネントを取得
+        // テキストの表示を入れ替える
+        myNameText.SetText(UserPublicScript.userName);
+
+        //クリアタイム
+        myTimeText.text =  UserPublicScript.clearTime.ToString();
+
+    }
+}
